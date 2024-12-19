72f74156cce840818b491b4c362fb957c49f55af
Valeriy Smirnov <valeriy.5mirnov@yandex.ru>
2024-12-19 09:42:28 +0700

    fixes after review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -26,7 +26,7 @@ SQLITE = {
     }
 }
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$')
 
 DEBUG = os.getenv('DEBUG') == True
 
